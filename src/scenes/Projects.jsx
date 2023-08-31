1c8be3615943364e468da833c62811f2da3c0788
Josephine Han <jsphnhan@gmail.com>
2023-08-30 17:36:09 -0700

    new pics

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -68,7 +68,7 @@ const Projects = () => {
         >
           {/* ROW 1 */}
           <div
-            className='flex justify-center text-center items-center p-10 bg-red
+            className='flex justify-center text-center items-center p-10 bg-yellow
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold'
           >
             BEAUTIFUL USER INTERFACES
